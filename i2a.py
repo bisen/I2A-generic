c1e58f15dddb93c9435bfce84b3e9551072d95a0
garnetliu <sliu@gm.slc.edu>
2018-12-13 11:59:23 -0500

    i2a update

--- i2a.py
+++ i2a.py
@@ -9,32 +9,48 @@ import a2c_agent
 MODEL_PATH_A2C = 'models/a2c/a2c_saved_model'
 MODEL_PATH_ENV = 'models/env/env_saved_model'
 
+BATCH_SIZE = 3
 
 # a standard nn of conv layers and fully connected one
 class I2A():
-    def __init__(self, game, in_shape, num_actions, num_rewards, num_rollouts, hidden_size, encoder, imagination, full_rollout=True):
+    def __init__(self, in_shape, num_actions, num_rewards, num_rollouts, hidden_size, encoder, imagination, full_rollout=True):
         self.state = tf.placeholder(tf.float32, shape=[None, None, in_shape[0], in_shape[1], in_shape[2]])
-        self.fc_in = tf.placeholder(tf.float32, shape=[num_rollouts, 238081])
-        self.x = tf.placeholder(tf.float32, shape=[None])
+        self.fc_in = tf.placeholder(tf.float32, shape=[num_rollouts, BATCH_SIZE*119040+1])
+        self.x = tf.placeholder(tf.float32, shape=[num_rollouts, 256])
+        self.reward = tf.placeholder(tf.float32, shape=[None, None, num_rewards])
+        self.actions = tf.placeholder(tf.int32, shape=[num_rollouts])
 
         self.in_shape = in_shape
         self.num_actions = num_actions
         self.num_rewards = num_rewards
         self.full_rollout = full_rollout
         self.num_rollouts = num_rollouts
+        self.hidden_size = hidden_size
 
-        self.features = self.set_features()
-        # self.critic = tf.layers.dense(self.x, 1)
-        # self.actor = tf.layers.dense(self.x, num_actions)
-
-        # self.imagination = ImaginationCore(num_rollouts, game.num_actions, 6, env, a2c)
-        # self.encoder = RolloutEncoder(num_rollouts, in_shape, num_rewards, hidden_size)
         self.imagination = imagination
         self.encoder = encoder
         self.fc = self.set_fc()
 
+        self.critic = tf.layers.dense(self.fc, 1)
+        self.actor = tf.nn.softmax(tf.layers.dense(self.fc, num_actions))
+
+        # self.imagination = ImaginationCore(num_rollouts, game.num_actions, 6, env, a2c)
+        # self.encoder = self.RolloutEncoder(num_rollouts, in_shape, num_rewards, hidden_size)
+
+        self.loss_val = self.loss()
+
         self.session = tf.Session()
         self.session.run(tf.global_variables_initializer())
+
+        # For saving/loading models
+        self.saver = tf.train.Saver(tf.global_variables())
+
+    # Load the last saved checkpoint during training or used by test
+    def load_last_checkpoint(self):
+        self.saver.restore(self.session, tf.train.latest_checkpoint('models/i2a/'))
+
+    def save_checkpoint(self):
+        self.saver.save(self.session, 'models/i2a/i2a_saved_model')
 
     # fully connected layer
     def set_fc(self):
@@ -44,44 +60,50 @@ class I2A():
         else:
             return tf.layers.dense(self.fc_in, output_size, activation=tf.nn.relu)
 
-    def set_features(self):
-        state = tf.reshape(self.state, [-1, self.in_shape[0], self.in_shape[1], self.in_shape[2]])
-        h0 = tf.layers.conv2d(state, filters=16, kernel_size=3, strides=1, activation=tf.nn.relu)
-        out = tf.layers.conv2d(h0, filters=16, kernel_size=3, strides=2, activation=tf.nn.relu)
-        return out
 
     def forward(self, states):
         batch_size = len(states)
-        imagined_state, imagined_reward = self.imagination.imagine(states)
-        feed_dict = {self.encoder.state: imagined_state, self.encoder.reward: imagined_reward}
-        hidden = self.session.run(self.encoder.encode(), feed_dict=feed_dict)
-
-        f = self.features()
-        f = tf.reshape(f, [self.num_rollouts, -1])
+        imagined_states, imagined_rewards, imagined_actions = self.imagination.imagine(states)
+        hidden = self.encoder.get_hidden(imagined_states, imagined_rewards)
+        f = self.encoder.get_f(imagined_states, imagined_rewards)
+        f = np.reshape(f, [hidden.shape[0], -1])
         # combine state
-        x = tf.concat([f, hidden], 1)
-        x = self.session.run(self.fc, feed_dict={self.fc_in:x})
+        x = np.concatenate([f, hidden], 1)
 
-        print(x.shape)
-        # logit = self.session.run(self.actor, feed_dict={self.x:x})
-        # value = self.session.run(self.critic, feed_dict={self.x:x})
+        # x = self.session.run(self.fc, feed_dict={self.fc_in:x})
+        logit = self.session.run(self.actor, feed_dict={self.fc_in:x})
+        value = self.session.run(self.critic, feed_dict={self.fc_in:x})
 
-        # return logit, value
+        return logit, value
+
+    def loss(self):
+        indicies = tf.range(0, tf.shape(self.actor)[0]) * 2 + self.actions
+        actProbs = tf.gather(tf.reshape(self.actor, [-1]), indicies)
+        advantage = self.reward[:,:1,1] - self.critic
+        cLoss = tf.reduce_mean(tf.square(advantage))
+        return cLoss
+
+    def optimize(self, dict):
+        self.session.run(tf.train.AdamOptimizer(.001).minimize(self.loss_val), feed_dict=dict)
 
 # rollout encoder composed from GRU
 class RolloutEncoder():
     def __init__(self, roll_steps, in_shape, num_rewards, hidden_size):
         # state shape: [batch_size, num_steps, width, height, depth]
-        self.state = tf.placeholder(tf.float32, shape=[None, None, in_shape[0], in_shape[1], in_shape[2]])
-        self.reward = tf.placeholder(tf.float32, shape=[None, None, num_rewards])
+        with tf.variable_scope('encoder', reuse=tf.AUTO_REUSE):
+            self.state = tf.placeholder(tf.float32, shape=[None, None, in_shape[0], in_shape[1], in_shape[2]])
+            self.reward = tf.placeholder(tf.float32, shape=[None, None, num_rewards])
 
-        self.roll_steps = roll_steps
-        self.hidden_size = hidden_size
-        self.in_shape = in_shape
-        self.num_rewards = num_rewards
+            self.roll_steps = roll_steps
+            self.hidden_size = hidden_size
+            self.in_shape = in_shape
+            self.num_rewards = num_rewards
 
-        self.features = self.set_features()
-        self.hidden_state = self.encode()
+            self.features = self.set_features()
+            self.hidden_state = self.encode()
+
+            self.session = tf.Session()
+            self.session.run(tf.global_variables_initializer())
 
     def set_features(self):
         state = tf.reshape(self.state, [-1, self.in_shape[0], self.in_shape[1], self.in_shape[2]])
@@ -96,6 +118,12 @@ class RolloutEncoder():
         rnn_input = tf.concat([features, self.reward], 2)
         _, next_state = tf.nn.dynamic_rnn(rnn, rnn_input, dtype=tf.float32)
         return next_state
+
+    def get_hidden(self, state, reward):
+        return self.session.run(self.hidden_state, feed_dict={self.state: state, self.reward: reward})
+
+    def get_f(self, state, reward):
+        return self.session.run(self.features, feed_dict={self.state: state, self.reward: reward})
 
 # imagination core (IC) predicts the next time step conditioned on an action sampled from the rollout policy π
 class ImaginationCore():
@@ -115,7 +143,7 @@ class ImaginationCore():
         states = np.array(normalize_states([[s, s] for s in states]))
         rollout_states = []
         rollout_rewards = []
-
+        rollout_actions = []
 
         if self.full_rollout:
             actions = np.array([[[i] for i in range(self.num_actions)] for j in
@@ -140,6 +168,7 @@ class ImaginationCore():
             s, r = self.convert_states_rewards(imagined_states, imagined_rewards, rollout_batch_size)
             rollout_states.append(s)
             rollout_rewards.append(r)
+            rollout_actions.append(actions)
 
             # reshape imagined state to a proper shape that can be passed into the environment
             imagined_states = np.reshape(imagined_states, (rollout_batch_size, 186, 160, 5))
@@ -153,7 +182,7 @@ class ImaginationCore():
                 for s in states:
                     actions.append(self.a2c.model.next_action(s))
 
-        return np.array(rollout_states), np.array(rollout_rewards)
+        return np.array(rollout_states), np.array(rollout_rewards), np.array(rollout_actions)
 
     def convert_states_rewards(self, imagined_states, imagined_rewards, rollout_batch_size):
         imagined_states = np.reshape(imagined_states, (rollout_batch_size, 186, 160, 5))
@@ -195,20 +224,27 @@ if __name__ == '__main__':
     a2c = ImportA2CGraph(pong.game)
     env = ImportEnvGraph(pong, MODEL_PATH_ENV)
 
-    session = tf.Session()
+    # session = tf.Session()
 
-    core = ImaginationCore(3, pong.num_actions, 6, env, a2c, False)
-    # imagined_states, imagined_rewards = core.imagine(np.array([a2c.model.game.reset(), a2c.model.game.reset()]))
+    num_rollout = 3
+    num_rewards = 6
+    image_dim = [186, 160, 3]
+    state = np.array([a2c.model.game.reset(), a2c.model.game.reset(), a2c.model.game.reset()])
 
-    encoder = RolloutEncoder(3, [186, 160, 3], 6, 1)
-    i2a = I2A(pong, in_shape=[186, 160, 3], num_actions=pong.num_actions, num_rewards=6, num_rollouts=2, hidden_size=1,
+    core = ImaginationCore(num_rollout, pong.num_actions, num_rewards, env, a2c, False)
+    encoder = RolloutEncoder(num_rollout, image_dim, num_rewards, 1)
+    # encoder.get_hidden(imagined_states, imagined_rewards)
+    i2a = I2A(in_shape=image_dim, num_actions=pong.num_actions, num_rewards=num_rewards, num_rollouts=num_rollout, hidden_size=1,
               encoder=encoder, imagination=core)
+    i2a.load_last_checkpoint()
+    logit, value = i2a.forward(state)
+    print('logit', logit)
+    print('value', value)
 
-    i2a.forward(np.array([a2c.model.game.reset(), a2c.model.game.reset()]))
-    # feed_dict = {encoder.state: imagined_states,
-    #              encoder.reward: imagined_rewards}
-    # session.run(tf.global_variables_initializer())
-    # h, f = session.run([encoder.hidden_state, encoder.features], feed_dict=feed_dict)
-    # print(h.shape, f.shape)
-    # x = np.concatenate((h, np.reshape(f, [3, -1])), axis=1)
-    # print(x.shape)
+    # imagined_states, imagined_rewards, imagined_actions = core.imagine(state)
+    # hidden = encoder.get_hidden(imagined_states, imagined_rewards)
+    # f = encoder.get_f(imagined_states, imagined_rewards)
+    # f = np.reshape(f, [hidden.shape[0], -1])
+    # # combine state
+    # x = np.concatenate([f, hidden], 1)
+    # i2a.optimize({i2a.fc_in: x, i2a.actions: imagined_actions[:, -1], i2a.reward: imagined_rewards})
